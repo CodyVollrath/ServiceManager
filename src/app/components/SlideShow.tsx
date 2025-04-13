1da089a5925172faeaac3ac8279a743695b4c1b5
CodyVollrath <950doenut.buff@gmail.com>
2025-04-12 20:22:52 -0400

    Adding colors

--- src/app/components/SlideShow.tsx
+++ src/app/components/SlideShow.tsx
@@ -3,28 +3,24 @@ import {useState, useEffect} from 'react';
 type Slide = {
     id: number,
     title: string,
-    description: string,
-    bgColor: string
+    description: string
 };
 
 const  defaultSlides: Slide[] = [
     {
         id: 1,
         title: "Welcome to ServiceBucket",
-        description: "Let us handle your project dependencies. You focus on the business needs!",
-        bgColor: "teal",
+        description: "Let us handle your project dependencies. You focus on the business needs!"
       },
       {
         id: 2,
         title: "Secure & Reliable",
         description: "Your data is protected with enterprise-grade security",
-        bgColor: "mediumturquoise",
       },
       {
         id: 3,
         title: "Join Our Community",
-        description: "Connect with thousands of innovators worldwide",
-        bgColor: "lightseagreen",
+        description: "Connect with thousands of innovators worldwide"
       },
 ];
 
@@ -40,7 +36,7 @@ export default function SlideShow({slides = defaultSlides }: {slides?: Slide[]})
     }, [slides.length]);
 
     return (
-      <div style={{ backgroundColor: slides[currentSlide].bgColor }}>
+      <div>
         <div key={slides[currentSlide].id} className="">
           <h2>{slides[currentSlide].title}</h2>
           <p>{slides[currentSlide].description}</p>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,15 +7,15 @@ import './styles.css';
 
 export default function LoginPage() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div className="flex w-full max-w-5xl rounded-lg shadow-lg overflow-hidden">
+    <div className="login-contianer h-screen flex items-center justify-center">
+      <div className="flex w-full h-full max-w-5xl rounded-lg shadow-lg overflow-hidden">
         {/* SlideShow Panel */}
-        <div className="flex bg-blue-500 items-center justify-center w-1/2 p-8">
+        <div className="flex slideShow items-center justify-center w-1/2 p-8">
           <SlideShow />
         </div>
 
         {/* Auth Panel */}
-        <div className="flex bg-red-500 items-center justify-center w-1/2 p-8">
+        <div className="flex authSide items-center justify-center w-1/2 p-8">
           <Auth />
         </div>
       </div>
